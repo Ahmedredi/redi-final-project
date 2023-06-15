bc24aaab764655bb2dea50bc083e562e272d2a40
Ahmedredi <129683331+Ahmedredi@users.noreply.github.com>
2023-06-15 17:06:40 +0200

    Redi project spring 2023

---
+++ 123.py
@@ -0,0 +1,54 @@
+Python 3.11.4 (tags/v3.11.4:d2340ef, Jun  7 2023, 05:45:37) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:\Users\ReDI User\Desktop\project 2.py
+Hello ,welcome to quiz
+would you like to play my quiz? yes
+Ok,lets play:
+What is CPU? Cetral Processing Unit
+Incorrect!
+ What does IT stand for? Informations Technology
+Correct!
+What is CPS? Central Power Supply
+Correct!
+What is the meaning of AI? Artifitial Inteligence
+Correct!
+What do you think about the Redi teachers? They are a great team
+Incorrect!
+
+=============== RESTART: C:\Users\ReDI User\Desktop\project 2.py ===============
+Hello ,welcome to quiz
+would you like to play my quiz? yes
+Ok,lets play:
+What is CPU? Central Processing Unit
+Correct!
+ What does IT stand for? Informations Technology
+Correct!
+What is CPS? Central Power Supply
+Correct!
+What is the meaning of AI? Artifial Inteligence
+Incorrect!
+What do you think about the Redi teachers? They are a great team!
+Correct!
+You got 4 questions correct!
+You got 0.80.80.80.80.80.80.80.80.80.80.80.80.80.80.80.80.80.80.80.80.80.80.80.80.80.80.80.80.80.80.80.80.80.80.80.80.80.80.80.80.80.80.80.80.80.80.80.80.80.80.80.80.80.80.80.80.80.80.80.80.80.80.80.80.80.80.80.80.80.80.80.80.80.80.80.80.80.80.80.80.80.80.80.80.80.80.80.80.80.80.80.80.80.80.80.80.80.80.80.8%
+>>> 
+=============== RESTART: C:\Users\ReDI User\Desktop\project 2.py ===============
+Hello ,welcome to quiz
+would you like to play my quiz? 
+=============== RESTART: C:\Users\ReDI User\Desktop\project 2.py ===============
+Hello ,welcome to quiz
+would you like to play my quiz? yes
+Ok,lets play:
+What is CPU? Central Processing Unit
+Correct!
+ What does IT stand for? Informations Technology
+Correct!
+What is CPS? Central Power Supply
+Correct!
+What is the meaning of AI? Artifitial Inteligence
+Correct!
+What do you think about the Redi teachers? They are a great team!
+Correct!
+You got 5 questions correct!
+You got 100.0%
